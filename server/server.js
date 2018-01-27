dbc4a6f4395e1f65d59eb3a9edda8eb929a5635a
shashvat <lanetteam.shashvat@gmail.com>
2018-01-27 18:03:57 +0530

    submitted on 27-1-18

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 const path=require('path');
+const http=require('http');
 const express=require('express');
+const socketIO = require('socket.io');
 
 const publicpath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 var app=express();
+var server=http.createServer(app);
+var io = socketIO(server);
 
-console.log(path.join(__dirname,'../public'));
+console.log(path.join(__dirname,' ../public'));
 
 app.use(express.static(publicpath));
 
-app.listen(3000,()=>{
+io.on('connection',(socket)=>{
+    console.log('New User connected');
+    socket.on('disconnect',()=>{
+        console.log("user was disconnected");
+    });
+});
+
+server.listen(port,()=>{
     console.log(`server started on ${port}`);
-})
+});
